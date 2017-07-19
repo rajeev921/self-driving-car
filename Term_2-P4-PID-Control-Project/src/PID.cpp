38d2a541e3bde5fbddb710b1e3f1edb2c47f46c4
rajeev921 <rajeev.cse.imps@gmail.com>
2017-07-19 15:54:03 +0530

    Adding MPC P5 Project

---
+++ Term_2-P4-PID-Control-Project/src/PID.cpp
@@ -0,0 +1,38 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+	this->Kp = Kp;
+	this->Ki = Ki;
+	this->Kd = Kd;
+
+	p_error = 0;
+	i_error = 0;
+	d_error = 0;
+
+}
+
+void PID::UpdateError(double cte) {
+	d_error  = p_error - cte;
+	i_error += cte;
+	p_error  = cte;
+}
+
+double PID::TotalError() {
+
+	double p = Kp * p_error;
+	double i = Ki * i_error;
+	double d = Kd * d_error;
+
+	return (-1 * (p + i + d));
+}
+
